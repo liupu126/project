318b7122f0a1bab66b33694ddd1376e48fa7b86d
Braden Liu <liupu126@126.com>
2019-08-01 18:25:49 +0800

    misc auto
    Signed-off-by: Braden Liu <liupu126@126.com>

---
+++ Scripts/bat/clrb.bat
@@ -0,0 +1,7 @@
+::bg5670
+adb reboot bootloader || fastboot reboot bootloader
+
+fastboot ops 4F50040TR18FTR7FSTD5F01
+::clear rollback index
+fastboot ops 4F50040TR18FTR7FSTD5F02
+fastboot set_active a
