0dd290435182af80b843124c09917cb8198e67a7
Braden Liu <liupu126@126.com>
2020-04-26 15:58:08 +0800

    misc auto
    Signed-off-by: Braden Liu <liupu126@126.com>

--- Scripts/bat/chkver.bat
+++ Scripts/bat/chkver.bat
@@ -1,5 +1,7 @@
 @echo off
 :: build & verified boot
-adb shell "getprop ro.boot.type && getenforce && getprop ro.boot.slot_suffix && getprop ro.boot.opcarrier && getprop ro.boot.verifiedbootstate && getprop vendor.boot.verifiedbootstate && getprop ro.boot.veritymode && getprop vendor.boot.veritymode"
+adb shell "getprop ro.boot.type && getenforce && getprop ro.boot.slot_suffix && getprop ro.boot.opcarrier && getprop gsm.current.phone-type && getprop ro.boot.verifiedbootstate && getprop vendor.boot.verifiedbootstate && getprop ro.boot.veritymode && getprop vendor.boot.veritymode"
 :: version: fingerprint & ota
 adb shell "getprop ro.system.build.fingerprint && getprop ro.product.build.fingerprint && getprop ro.vendor.build.fingerprint && getprop persist.sys.version.lastota && getprop persist.sys.version.ota && getprop ro.build.ota.versionname"
+:: others
+adb shell "getprop ro.build.version.security_patch && getprop ro.vendor.build.security_patch && getprop ro.com.google.gmsversion"
