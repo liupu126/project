94955891fd5da29eb7b9c27a48e6aa509aa8768e
Braden Liu <liupu126@126.com>
2020-07-24 09:59:36 +0800

    misc auto
    Signed-off-by: Braden Liu <liupu126@126.com>

--- Scripts/bat/opkey_attest.bat
+++ Scripts/bat/opkey_attest.bat
@@ -1,5 +1,5 @@
 ::For example:
-:: opkey OP7ProNRSpr.xml
+:: opkey_attest OP7ProNRSpr.xml
 
 :: for comparison
 set null=
@@ -11,20 +11,21 @@ set last_param=%1
 
 set attestation_file=%1
 
+
+
 ::RPMB
 adb wait-for-device
 adb shell dumpsys android.security.keystore --enable_rpmb
 adb reboot
-
 ping 127.0.0.1 -n 5 > nul
 adb wait-for-device
-
-::Keymaster Attestation
 ping 127.0.0.1 -n 20 > nul
 
+
+
+::Keymaster Attestation
 :: I mv      : type=1400 audit(0.0:337): avc: denied { setattr } for name=".lii" dev="sda7" ino=32772 scontext=u:r:vold:s0 tcontext=u:object_r:op2_file:s0 tclass=dir permissive=0
 adb shell setenforce 0
-
 adb shell mkdir -p /sdcard/.lii
 adb push %attestation_file% /sdcard/.lii/keyboxes_ciphered.xml
 :: Android P
